36984f925a187fee8dc7d8cc7891ef4a4ca27b4f
Tobias Tonndorff <cph-tt160@cphbusiness.dk>
2022-11-23 17:26:47 +0100

    programkontrol

--- src/FileIO.java
+++ src/FileIO.java
@@ -148,5 +148,5 @@ public class FileIO {
         }
         return listOfCategories;
     }
-    
+
 }
---
+++ src/ProgramControl.java
@@ -0,0 +1,34 @@
+
+import java.util.ArrayList;
+public class ProgramControl {
+    private ArrayList<User> UsersList = new ArrayList<>();
+    private ArrayList<AMedia> mediaList = new ArrayList<>();
+    public static User defaultUser;
+    public static MainMenu mainMenu;
+
+    public programcontrol(){
+
+    }
+    public void runProgram(){
+        ArrayList<AMedia> movieList = FileIO.readMovieFile("Data/Movies.csv");
+        ArrayList<AMedia> seriesList = FileIO.readSeriesFile("Data/Series.csv");
+        for(AMedia m : movieList){
+            mediaList.add(m);
+        }
+        for(AMedia m: seriesList){
+            mediaList.add(m);
+        }
+        UsersList = FileIO.readUsersFile("Data/Users.csv", mediaList);
+
+        StartMenu startMenu = new StartMenu(UsersList);
+        defaultUser = startMenu.exeStartMenu();
+        if(!UsersList.contains(defaultUser)){
+            UsersList.add(defaultUser);
+        }
+        mainMenu = new MainMenu(UsersList, movieList, seriesList);
+        mainMenu.runMainMenu();
+
+        FileIO.writeUsersFile("Data/Users.csv", UsersList);
+
+    }
+}
